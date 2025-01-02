9dc4db76608c2d7bf2d92b0d749e61be8ce4ce3e
Frotas <89676387+Frotas@users.noreply.github.com>
2025-01-01 21:00:26 -0300

    refactor: :lipstick: Ajuste do aredondamento das bordar do `Skeleton`

--- src/shared/components/molecules/skeleton/index.tsx
+++ src/shared/components/molecules/skeleton/index.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/shared/utils/cn"
 function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
 	return (
 		<div
-			className={cn("animate-pulse rounded-md bg-zinc-300/10", className)}
+			className={cn("animate-pulse rounded-lg bg-zinc-300/10", className)}
 			{...props}
 		/>
 	)
