381e6cfc80698ebe5f31f9997833ab06b0d1c612
Frotas <89676387+Frotas@users.noreply.github.com>
2025-01-02 21:59:23 -0300

    refactor(mock/api): :sparkles: Ajuste das rotas de autenticação e implementação das rotas de perfis
    ### Modificações (refactor)
    - `auth/api/getAuth.ts`: Colocação de um `ID` fixo provisório.
    - `mocks/handlers.ts`: Modificação das rotas para utilizar o `JSON-Server` e estar de acordo com a estrutura de uma `API Rest`.
    - `mocks/index.ts`: Remoção da instrução `await` por recomendação do `Next.js`.
    
    ### Adições (feat)
    - `mocks/localDB.json`: Criação da estrutura do arquivo de banco de dados e rotas do `JSON-Server`.

--- src/shared/libs/mocks/handlers.ts
+++ src/shared/libs/mocks/handlers.ts
@@ -1,17 +1,137 @@
-// src/mocks/handlers.js
-import { http, HttpResponse } from "msw"
+import { randomUUID } from "crypto"
 
-const localhost = process.env.NEXT_PUBLIC_DB_HOST
+import { http, HttpResponse, type PathParams } from "msw"
+
+import { type ProfileProps } from "@/shared/schemas/Profile.schema"
+import { type UserProps } from "@/shared/schemas/UserProps"
+
+const localhost = String(process.env.NEXT_PUBLIC_DB_HOST)
+const localDB = String(process.env.MSW_LOCAL_DATABASE_URL || "http://localhost:3005")
 
 export const handlers = [
-	// Intercept "GET https://example.com/user" requests...
-	http.get(localhost + "/auth", () => {
-		// ...and respond to them using this JSON response.
-		return HttpResponse.json({
-			id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",
-			name: "john cena",
-			email: "johncena@gmail.com",
-			avatar: "https://github.com/shadcn.png"
-		})
-	})
+	// auth Routes
+	http.post<PathParams, UserProps, any>(`${localhost}/auth`, async ({ request }) => {
+		try {
+			const data = await request.json()
+			const id = String(randomUUID().split("-", 1).join())
+			const res = await fetch(`${localDB}/users`, {
+				method: "POST",
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({ ...data, id: id })
+			})
+			const content: UserProps = await res.json()
+			return HttpResponse.json<UserProps>(content)
+		} catch (error) {
+			if (error instanceof Error) {
+				return HttpResponse.json(JSON.stringify({ error: error.message }), {
+					status: 403
+				})
+			}
+			return HttpResponse.json(JSON.stringify(error), { status: 403 })
+		}
+	}), // Create User
+	http.get<PathParams, UserProps, any>(`${localhost}/auth/:id`, async ({ params }) => {
+		try {
+			const { id } = params
+			const res = await fetch(`${localDB}/users/${id}`)
+			const content: UserProps = await res.json()
+			return HttpResponse.json<UserProps>(content)
+		} catch (error) {
+			if (error instanceof Error) {
+				return HttpResponse.json(JSON.stringify({ error: error.message }), {
+					status: 403
+				})
+			}
+			return HttpResponse.json(JSON.stringify(error), { status: 403 })
+		}
+	}), // Get User
+
+	// profile Routes
+	http.get<PathParams, ProfileProps, any>(`${localhost}/profile`, async () => {
+		try {
+			const request = await fetch(`${localDB}/profile`)
+			const content: ProfileProps[] = await request.json()
+			return HttpResponse.json<ProfileProps[]>(content)
+		} catch (error) {
+			if (error instanceof Error) {
+				return HttpResponse.json(JSON.stringify({ error: error.message }), {
+					status: 403
+				})
+			}
+			return HttpResponse.json(JSON.stringify(error), { status: 403 })
+		}
+	}), // Get All Profiles
+	http.get<PathParams, ProfileProps, any>(
+		`${localhost}/profile/:id`,
+		async ({ params }) => {
+			try {
+				const { id } = params
+				const request = await fetch(`${localDB}/profiles/${id || "3fb746a9"}`)
+				const content: ProfileProps = await request.json()
+				return HttpResponse.json<ProfileProps>(content)
+			} catch (error) {
+				if (error instanceof Error) {
+					return HttpResponse.json(JSON.stringify({ error: error.message }), {
+						status: 403
+					})
+				}
+				return HttpResponse.json(JSON.stringify(error), { status: 403 })
+			}
+		}
+	), // Get Profile by ID
+	http.post<PathParams, ProfileProps, any>(
+		`${localhost}/profile`,
+		async ({ request }) => {
+			try {
+				const data = await request.json()
+				const id = String(randomUUID().split("-", 1).join())
+				const res = await fetch(`${localDB}/profiles`, {
+					method: "POST",
+					headers: {
+						Accept: "application/json",
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({ ...data, id: id })
+				})
+				const content = await res.json()
+				return HttpResponse.json<ProfileProps>(content)
+			} catch (error) {
+				if (error instanceof Error) {
+					return HttpResponse.json(JSON.stringify({ error: error.message }), {
+						status: 403
+					})
+				}
+				return HttpResponse.json(JSON.stringify(error), { status: 403 })
+			}
+		}
+	), // Create Profile
+	http.put<PathParams, ProfileProps>(
+		`${localhost}/profile/update/:id`,
+		async ({ params, request }) => {
+			try {
+				const { id } = params
+				const data = await request.json()
+				const res = await fetch(`${localDB}/profiles/${id}`, {
+					method: "PUT",
+					headers: {
+						Accept: "application/json",
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(data)
+				})
+				const content = await res.json()
+				return HttpResponse.json(content)
+			} catch (error) {
+				if (error instanceof Error) {
+					return HttpResponse.json(JSON.stringify({ error: error.message }), {
+						status: 403
+					})
+				}
+				return HttpResponse.json(JSON.stringify(error), { status: 403 })
+			}
+		}
+	) // Update Profile
 ]
