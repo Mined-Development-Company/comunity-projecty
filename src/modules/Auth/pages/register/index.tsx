99a59f087096904247a156f95ff2ddf186e47ad0
0rigerinho <luaansilva11@gmail.com>
2025-11-17 21:09:45 -0300

    correÃ§Ã£o, melhoria e adiÃ§Ã£o de melhor dps e tank em Battleground

--- src/modules/Auth/pages/register/index.tsx
+++ src/modules/Auth/pages/register/index.tsx
@@ -2,18 +2,38 @@
 
 import React from "react"
 
+import { useRouter } from "nextjs-toploader/app"
+
 import { AuthCard } from "@/modules/Auth/components/AuthCard"
 import { useModel } from "@/modules/Auth/pages/hooks/useModel"
+import type { UserProps } from "@/shared/@types/UserProps"
 
 export default function Register() {
 	const { isLoading, setEnable } = useModel()
+
+	const router = useRouter()
+
+	const fakeUser: UserProps = {
+		id: "1",
+		name: "John Doe",
+		email: "john.doe@example.com",
+		avatar: "https://github.com/shadcn.png",
+		profileId: "1"
+	}
+
+	function handleClickGithub() {
+		setEnable((prev) => !prev)
+		localStorage.setItem("userData", JSON.stringify(fakeUser))
+		router.push("/")
+	}
+
 	return (
 		<div className="flex flex-1 items-center justify-center">
 			<AuthCard
 				action="register"
 				isLoading={isLoading}
-				onClickDiscord={() => setEnable((prev) => !prev)}
-				onClickGithub={() => setEnable((prev) => !prev)}
+				onClickDiscord={handleClickGithub}
+				onClickGithub={handleClickGithub}
 			/>
 		</div>
 	)
