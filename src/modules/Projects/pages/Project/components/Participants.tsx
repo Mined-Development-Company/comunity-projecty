7b842ae0dd7eec8d4c00111492f20a3985ed25e4
0rigerinho <luaansilva11@gmail.com>
2025-12-25 18:48:49 -0300

    correção de merge

---
+++ src/modules/Projects/pages/Project/components/Participants.tsx
@@ -0,0 +1,66 @@
+import { Box } from "@/shared/components/atoms/box"
+import { AvatarDefault } from "@/shared/components/molecules/avatars/AvatarDefault"
+
+const avatars = [
+	{
+		src: "https://zanotti.com.br/blog/wp-content/uploads/2016/08/Modelo-Victoria-Secrets-Adriana-Lima.jpg",
+		name: "0"
+	},
+	{
+		src: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Gisele_B%C3%BCndchen_em_junho_de_2006.jpg/800px-Gisele_B%C3%BCndchen_em_junho_de_2006.jpg",
+		name: "1"
+	},
+	{
+		src: "https://zanotti.com.br/blog/wp-content/uploads/2016/08/Modelo-Victoria-Secrets-Adriana-Lima.jpg",
+		name: "2"
+	},
+	{
+		src: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Gisele_B%C3%BCndchen_em_junho_de_2006.jpg/800px-Gisele_B%C3%BCndchen_em_junho_de_2006.jpg",
+		name: "3"
+	},
+	{
+		src: "https://zanotti.com.br/blog/wp-content/uploads/2016/08/Modelo-Victoria-Secrets-Adriana-Lima.jpg",
+		name: "4"
+	},
+	{
+		src: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Gisele_B%C3%BCndchen_em_junho_de_2006.jpg/800px-Gisele_B%C3%BCndchen_em_junho_de_2006.jpg",
+		name: "5"
+	},
+	{
+		src: "https://zanotti.com.br/blog/wp-content/uploads/2016/08/Modelo-Victoria-Secrets-Adriana-Lima.jpg",
+		name: "6"
+	},
+	{
+		src: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Gisele_B%C3%BCndchen_em_junho_de_2006.jpg/800px-Gisele_B%C3%BCndchen_em_junho_de_2006.jpg",
+		name: "7"
+	},
+	{
+		src: "https://zanotti.com.br/blog/wp-content/uploads/2016/08/Modelo-Victoria-Secrets-Adriana-Lima.jpg",
+		name: "8"
+	},
+	{
+		src: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Gisele_B%C3%BCndchen_em_junho_de_2006.jpg/800px-Gisele_B%C3%BCndchen_em_junho_de_2006.jpg",
+		name: "9"
+	},
+	{
+		src: "https://zanotti.com.br/blog/wp-content/uploads/2016/08/Modelo-Victoria-Secrets-Adriana-Lima.jpg",
+		name: "10"
+	},
+	{
+		src: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Gisele_B%C3%BCndchen_em_junho_de_2006.jpg/800px-Gisele_B%C3%BCndchen_em_junho_de_2006.jpg",
+		name: "11"
+	}
+]
+
+export function Participants() {
+	return (
+		<Box className="space-y-3">
+			<h3 className="font-bold text-content-primary">Participantes</h3>
+			<div className="flex flex-wrap gap-3">
+				{avatars.map(({ src }, index) => (
+					<AvatarDefault key={index} src={src} size="sm" />
+				))}
+			</div>
+		</Box>
+	)
+}
---
+++ src/modules/Projects/pages/Project/components/Registration.tsx
@@ -0,0 +1,77 @@
+"use client"
+
+import { Box } from "@/shared/components/atoms/box"
+import { Button } from "@/shared/components/atoms/button"
+import { Checkbox } from "@/shared/components/atoms/checkbox"
+import { Label } from "@/shared/components/atoms/label"
+import { RadioGroup, RadioGroupItem } from "@/shared/components/atoms/radioGroup"
+
+const checkeds = [
+	"Aprender novas habilidades",
+	"Ter um projeto no portfólio",
+	"Fazer networking",
+	"Contribuir com a comunidade",
+	"Outro"
+]
+const radios = [
+	"Redes sociais",
+	"Indicação de amigos ou colegas",
+	"Site ou blog",
+	"Outro"
+]
+
+export function Registration() {
+	return (
+		<Box>
+			<form action="" className="space-y-6">
+				<div>
+					<p className="text-xl font-bold text-content-primary">
+						Formulário de Participação
+					</p>
+					<p className="text-xs text-content-quaternary">
+						Por favor, preencha as informações abaixo para participar do projeto.
+					</p>
+				</div>
+
+				<div className="space-y-3">
+					<p className="text-sm font-semibold text-content-primary">
+						Por que você quer participar do projeto?
+					</p>
+					<div className="space-y-3">
+						{checkeds.map((value) => (
+							<div className="flex items-center space-x-2" key={value}>
+								<Checkbox id="terms" />
+								<label
+									htmlFor="terms"
+									className="text-sm font-medium leading-none text-content-quaternary peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+									{value}
+								</label>
+							</div>
+						))}
+					</div>
+				</div>
+
+				<div className="space-y-3">
+					<p className="text-sm font-semibold text-content-primary">
+						Como você descobriu este projeto?
+					</p>
+
+					<RadioGroup className="space-y-1">
+						{radios.map((value, index) => (
+							<div className="flex items-center space-x-2" key={index}>
+								<RadioGroupItem value={value} id={value} />
+								<Label className="text-content-quaternary" htmlFor={value}>
+									{value}
+								</Label>
+							</div>
+						))}
+					</RadioGroup>
+				</div>
+
+				<Button className="w-full font-semibold" type="submit">
+					Participar do projeto
+				</Button>
+			</form>
+		</Box>
+	)
+}
